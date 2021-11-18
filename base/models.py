21a86c65c54e3ff632040e7ac90c1d4e5a390610
Parth <mittalparth22@gmail.com>
2021-11-18 23:06:01 +0530

    added employee model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    GENDER = (('0', 'Female'), ('1', 'Male'), ('2', 'Other'))
+    name = models.CharField(max_length = 50)
+    age = models.IntegerField()
+    gender = models.CharField(max_length = 20, null=True, choices = GENDER)
+    address = models.CharField(max_length = 300)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=12)
+    date_of_joining = models.DateField(auto_now_add = False)
+
+    def __str__(self):
+        return self.name
