20bbc0bdaedd61bf3613f1b87a02f4505321980c
Parth <mittalparth22@gmail.com>
2021-11-20 22:53:33 +0530

    add department to models

--- base/models.py
+++ base/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length =100)
+
+    def __str__(self):
+        return self.name
+
 class Employee(models.Model):
-    GENDER = (('0', 'Female'), ('1', 'Male'), ('2', 'Other'))
+    FEMALE = 1
+    MALE = 2
+    OTHER = 3
+    GENDER_CHOICES = ((FEMALE, 'Female'), (MALE, 'Male'), (OTHER, 'Other'))
     name = models.CharField(max_length = 50)
     age = models.IntegerField()
-    gender = models.CharField(max_length = 20, null=True, choices = GENDER)
+    gender = models.IntegerField(null=True, choices = GENDER_CHOICES)
     address = models.CharField(max_length = 300)
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=12)
     date_of_joining = models.DateField(auto_now_add = False)
+    department = models.ForeignKey(Department, null=True, on_delete= models.SET_NULL)
 
     def __str__(self):
         return self.name
+
