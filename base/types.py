ef87bf70ae84ea47a0e986c2a43f5ee0a129a509
Parth <mittalparth22@gmail.com>
2021-11-22 22:30:51 +0530

    added types.py

--- base/types.py
+++ base/types.py
@@ -11,18 +11,3 @@ class DepartmentType(DjangoObjectType):
     class Meta:
         model = Department
         fields = '__all__'
-
-class EmployeeInput(graphene.InputObjectType):
-    id = graphene.ID()
-    name = graphene.String(required=True)
-    age = graphene.Int()
-    gender = graphene.Int()
-    address = graphene.String()
-    email = graphene.String(required=True)
-    phone = graphene.String()
-    date_of_joining = graphene.Date(required=True)
-    department = graphene.Field(graphene.ID)
-
-class DepartmentInput(graphene.InputObjectType):
-    id = graphene.ID()
-    name = graphene.String(required=True)
